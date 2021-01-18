43c4e6ce2ab7e4207c5f19ef13d005e447e06526
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-17 17:14:04 -0800

    reverseint() completed

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,20 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let str = n.toString();
+    let res = "";
+
+    for(let char of str){
+        res = char + res;
+    }
+
+    if(res[res.length - 1] === "-"){
+        res.slice(res.length - 1);
+        res = "-" + res;
+    }
+
+    return parseInt(res);
+}
 
 module.exports = reverseInt;
