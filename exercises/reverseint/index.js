533de669f764a82cdc201ad7ecdf976673dae24d
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-01-24 18:17:31 -0800

    other solution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -25,3 +25,12 @@ function reverseInt(n) {
 }
 
 module.exports = reverseInt;
+
+// function reverseInt(n){
+//     let reversed = n.toString().split('').reverse().join('');
+
+//     if(n < 0){
+//         return parseInt(reversed) * -1;
+//     }
+//     return parseInt(reversed);
+// }
