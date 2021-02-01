9786659c33a200f1f26304d0c838cf2e9ac3250e
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-02-01 15:59:54 -0800

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let map = {};
+    let max = 0;
+    let maxChar = '';
+    
+    for(let char of str){
+        map[char] = map[char] + 1 || 1;
+    }
+
+    for(let char in map){
+        if(map[char] > max){
+            max = map[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
