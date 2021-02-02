519583c64806042355317990520ec0da56df6129
George Allison <49288165+joji-harada@users.noreply.github.com>
2021-02-01 16:04:51 -0800

    alternate solution to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,16 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {
-    let chunked = [];
+// function chunk(array, size) {
+//     let chunked = [];
     
-    for(let element of array){
-        let last = chunked[chunked.length - 1];
-        if(!last || last.length === size){
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+//     for(let element of array){
+//         let last = chunked[chunked.length - 1];
+//         if(!last || last.length === size){
+//             chunked.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+// }
+function chunk(array, size){
+    let chunked = [];
+    let index = 0;
+    while(index < array.length){
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
     return chunked;
 }
